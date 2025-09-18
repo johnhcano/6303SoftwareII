d8675e403a196fe2266480884930fef0148d51f2
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-09-18 13:49:44 +0000

    Creación de la herencia con Builder

---
+++ PatronesDeDiseno/Builder/Converter.java
@@ -0,0 +1,17 @@
+public class Converter {
+
+    private String document;
+
+    public void makeLine(){
+    }
+
+    public void makeParagraph(){
+    }
+
+    public void makeTable(){
+    }
+
+    public String getDocument(){
+        return document;
+    }
+}
